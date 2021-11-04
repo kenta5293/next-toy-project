2edaea0783ab0f28464215029e7801c62b57f9ce
배건태 <kenta.03.1110@gmail.com>
2021-11-04 10:15:33 +0900

    :package: [Setting] Storybook 세팅 [1]
    디자인가이드와 컴포넌트 관리를 더 세밀하게 해보기 위해서 Storybook 공부를 위해 세팅.

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../components/**/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+}
