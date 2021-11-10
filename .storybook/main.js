a63d7fd91910bd1cfbfe6e681518a7de3b2b98c2
배건태 <kenta.03.1110@gmail.com>
2021-11-10 18:57:15 +0900

    :bulb: [Comment] main.js 설명 주석문 작성 및 docs 애드온 추가
    main.js에 대한 내용 주석문으로 작성 및 addon에 docs 문서 작성 시 도움이 되는 @storybook/addon-docs 추가.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,16 @@
 module.exports = {
   "stories": [
-    "../components/**/index.stories.@(js|jsx|ts|tsx)"
+    "../components/**/index.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs"
   ],
-} 
+}
+/*
+  ⭐️ main.js
+   ∙ Storybook을 위한 config 설정이 담겨 있음.
+   ∙ stories : story 파일이 프로젝트 내 어디에 어떤 형식의 파일로 위치하고 있는 지 명시.
+   ∙ addons : Storybook에서 사용되는 plugin 시스템.
+*/
